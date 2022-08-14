d68ac8b3ef0f3686bbe8a0acf4d62e6df18ede8c
Aman Choudhary <59244443+amanchoudhary8@users.noreply.github.com>
2022-08-14 19:10:21 +0530

    Time: 460 ms (76.94%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 Select name as Customers
 From Customers 
-where not exists (select * from Orders where Orders.CustomerId=Customers.id);
+where not exists (select * from Orders where Orders.CustomerId=Customers.id limit 1);
