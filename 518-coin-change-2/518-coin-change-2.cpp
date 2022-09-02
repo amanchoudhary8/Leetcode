0e28a4ac7258877bc37b307c20b06fd8c1dccb9d
Aman Choudhary <59244443+amanchoudhary8@users.noreply.github.com>
2022-09-02 09:33:45 +0530

    Time: 68 ms (27.92%), Space: 7.2 MB (82.26%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
-    int help(int n,int amount, vector<int>& coins,vector<vector<int>> &dp){
-        if(amount==0) return 1;
-        if(n==0) return 0;
-        if(dp[n][amount]!=-1) return dp[n][amount];
+//     int help(int n,int amount, vector<int>& coins,vector<vector<int>> &dp){
+//         if(amount==0) return 1;
+//         if(n==0) return 0;
+//         if(dp[n][amount]!=-1) return dp[n][amount];
         
-        int nott=help(n-1,amount,coins,dp);
-        int take=0;
-        if(amount>=coins[n-1]) take=help(n,amount-coins[n-1],coins,dp);
+//         int nott=help(n-1,amount,coins,dp);
+//         int take=0;
+//         if(amount>=coins[n-1]) take=help(n,amount-coins[n-1],coins,dp);
         
-        return dp[n][amount]=take+nott;
+//         return dp[n][amount]=take+nott;
         
-    }
+//     }
     
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
-        return help(n,amount,coins,dp);
+        vector<int> dp(amount+1,0);
+        
+        dp[0]=1;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                int nott=dp[j];
+                int take=0;
+                if(j>=coins[i-1]) take=dp[j-coins[i-1]];
+
+                dp[j]=take+nott;
+            }
+        }
+        return dp[amount];
     }
 };
