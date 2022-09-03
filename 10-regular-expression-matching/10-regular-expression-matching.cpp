430797bcc1fa7417d35981c30e450435fe121af2
Aman Choudhary <59244443+amanchoudhary8@users.noreply.github.com>
2022-09-03 19:28:17 +0530

    Time: 3 ms (91.44%), Space: 7.4 MB (29.88%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -14,8 +14,7 @@ public:
         if(p[i-1]=='*'){
             bool opt1=f(i-2,j,p,s,dp);
             bool opt2=0;
-            if(p[i-2]=='.') opt2=f(i,j-1,p,s,dp);
-            else if(p[i-2]==s[j-1]) opt2=f(i,j-1,p,s,dp);
+            if(p[i-2]=='.' || (p[i-2]==s[j-1])) opt2=f(i,j-1,p,s,dp);
             return dp[i][j]=opt1 || opt2;
         }
         return dp[i][j]=0;
@@ -23,7 +22,26 @@ public:
     
     bool isMatch(string s, string p) {
         int m=p.length(),n=s.length();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return f(m,n,p,s,dp);
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        dp[0][0]=1;
+        for(int i=1;i<=m;i++){
+            if(i&1) dp[i][0]=0;
+            else dp[i][0]=dp[i-2][0] & (p[i-1]=='*');
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(p[i-1]==s[j-1] || p[i-1]=='.') dp[i][j]=dp[i-1][j-1];
+                else if(p[i-1]=='*'){
+                    bool opt1=dp[i-2][j];
+                    bool opt2=0;
+                    if(p[i-2]=='.' || (p[i-2]==s[j-1])) opt2=dp[i][j-1];
+                    dp[i][j]=opt1 || opt2;
+                }
+                else dp[i][j]=0; 
+            }
+        }
+        
+        
+        return dp[m][n];
     }
 };
