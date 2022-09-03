09c20b8155443f51ff49dcad6be88ac878a22692
Aman Choudhary <59244443+amanchoudhary8@users.noreply.github.com>
2022-09-03 17:26:05 +0530

    Time: 137 ms (28.39%), Space: 27.1 MB (41.76%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -21,7 +21,20 @@ public:
     
     bool isMatch(string s, string p) {
         int m=p.length(),n=s.length();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return f(m,n,p,s,dp);
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        dp[0][0]=1;
+        for(int j=1;j<=n;j++) dp[0][j]=0;
+        for(int i=1;i<=m;i++)
+            dp[i][0]=dp[i-1][0] & (p[i-1]=='*');
+        
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if((p[i-1]==s[j-1]) || (p[i-1]=='?'))  dp[i][j]=dp[i-1][j-1];
+                else if(p[i-1]=='*') dp[i][j]=(dp[i-1][j] || dp[i][j-1]);
+                else dp[i][j]=0;
+            }
+        }        
+        return dp[m][n];
     }
 };
