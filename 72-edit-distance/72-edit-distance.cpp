f82ffa896efe22477412198d4cec783afcf1a0e0
Aman Choudhary <59244443+amanchoudhary8@users.noreply.github.com>
2022-09-03 09:30:44 +0530

    Time: 15 ms (73.92%), Space: 6.6 MB (90.90%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -16,8 +16,25 @@ public:
     
     int minDistance(string word1, string word2) {
         int m=word1.length(),n=word2.length();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return f(m,n,word1,word2,dp);
+        vector<vector<int>> dp(2,vector<int>(n+1,0));//2 rows only
+        
+        for(int j=0;j<=n;j++) dp[0][j]=j;
+        
+        for(int i=1;i<=m;i++){
+            dp[i&1][0]=i;
+            for(int j=1;j<=n;j++){
+                 if(word1[i-1]==word2[j-1]) dp[i&1][j]=dp[(i-1)&1][j-1];
+                 else{
+                    int opt1=dp[(i-1)&1][j];
+                    int opt2=dp[(i-1)&1][j-1];
+                    int opt3=dp[i&1][j-1];
+                     
+                     dp[i&1][j]=1+min(opt1,min(opt2,opt3));
+                 }
+            }
+        }
+        
+        return dp[m&1][n];
         
     }
 };
