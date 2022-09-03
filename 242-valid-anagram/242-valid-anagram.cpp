7f42607ec8e6ac2914a12221899ca69a5e840b49
Aman Choudhary <59244443+amanchoudhary8@users.noreply.github.com>
2022-09-03 17:51:14 +0530

    Time: 17 ms (44.80%), Space: 7.1 MB (99.38%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
+    bool isAnagram(string &s, string &t) {
         vector<int> v(26,0);
-        for(auto x:s) v[x-97]++;
-        for(auto x:t) v[x-97]--;
+        for(auto &x:s) v[x-97]++;
+        for(auto &x:t) v[x-97]--;
         
-        for(auto x:v) if(x!=0) return 0;
+        for(auto &x:v) if(x) return 0;
         return 1;
     }
 };
