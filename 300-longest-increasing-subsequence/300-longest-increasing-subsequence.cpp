dda18e59c9af86d5c4bd1151a2331cd115b925fe
Aman Choudhary <59244443+amanchoudhary8@users.noreply.github.com>
2022-09-04 21:33:17 +0530

    Time: 396 ms (47.27%), Space: 10.6 MB (44.63%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,29 +1,71 @@
+// class Solution {
+// public:
+//     int lis(int ind,int prev,vector<int>& nums,vector<vector<int>> &dp){
+//         if(ind==nums.size()) return 0;
+        
+//         if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
+//         int nott=0+lis(ind+1,prev,nums,dp);
+//         int tk=0;
+//         if(prev==-1 || nums[prev]<nums[ind]) tk=1+lis(ind+1,ind,nums,dp);
+//         return dp[ind][prev+1]=max(nott,tk);
+//     }
+    
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int> next(n+1,0),cur(n+1,0);
+        
+//         for(int ind=n-1;ind>=0;ind--){
+//             for(int prev=-1;prev<ind;prev++){
+//                 int nott=0+next[prev+1];
+//                 int tk=0;
+//                 if(prev==-1 || nums[prev]<nums[ind]) tk=1+next[ind+1];
+//                 cur[prev+1]=max(nott,tk);
+//             }
+//             next=cur;
+//         }
+        
+//         return next[-1 + 1];
+//     }
+// };
+
+
+
 class Solution {
 public:
-    int lis(int ind,int prev,vector<int>& nums,vector<vector<int>> &dp){
-        if(ind==nums.size()) return 0;
-        
-        if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
-        int nott=0+lis(ind+1,prev,nums,dp);
-        int tk=0;
-        if(prev==-1 || nums[prev]<nums[ind]) tk=1+lis(ind+1,ind,nums,dp);
-        return dp[ind][prev+1]=max(nott,tk);
-    }
-    
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<int> next(n+1,0),cur(n+1,0);
+        vector<int> v(n,1);
+        for(int i=1;i<n;i++)
+            for(int j=0;j<i;j++)
+                if(nums[j]<nums[i]) v[i]=max(v[i],v[j]+1);
+            
         
-        for(int ind=n-1;ind>=0;ind--){
-            for(int prev=-1;prev<ind;prev++){
-                int nott=0+next[prev+1];
-                int tk=0;
-                if(prev==-1 || nums[prev]<nums[ind]) tk=1+next[ind+1];
-                cur[prev+1]=max(nott,tk);
-            }
-            next=cur;
-        }
-        
-        return next[-1 + 1];
+        int ans=1;
+        for(auto &x:v) ans=max(ans,x);
+        return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
