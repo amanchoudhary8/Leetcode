b2cbdf86bbc7720d33d34656bad00902476c0246
Aman Choudhary <59244443+amanchoudhary8@users.noreply.github.com>
2022-09-12 21:57:31 +0530

    Time: 653 ms (53.08%), Space: 10.4 MB (18.71%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -13,9 +13,19 @@ public:
     
     int maxCoins(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
         nums.push_back(1);
         nums.insert(nums.begin(),1);
-        return help(1,n,nums,dp);
+        
+        for(int i=n;i>=1;i--){
+            for(int j=i;j<=n;j++){
+                int ans=INT_MIN;
+                for(int k=i;k<=j;k++)
+                    ans=max(ans,nums[i-1]*nums[j+1]*nums[k]+dp[i][k-1]+dp[k+1][j]);
+
+                dp[i][j]=ans;
+            }
+        }        
+        return dp[1][n];        
     }
 };
